b19f184ff6decbf97f82f4f6eec406c175d43ce2
clien9025 <302677599@qq.com>
2023-11-19 17:39:16 +0800

    init frist

---
+++ src/main/java/com/example/springbootwebserver/SpringbootWebServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootwebserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootwebserver/SpringbootWebServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootwebserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
