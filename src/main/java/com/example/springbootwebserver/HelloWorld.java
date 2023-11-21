1114c8cbd3c4dd0ca9c75bd74219c96de899eee7
clien9025 <302677599@qq.com>
2023-11-21 15:40:22 +0800

    pom修改成最简洁配置,并且上传成功

--- src/main/java/com/example/springbootwebserver/HelloWorld.java
+++ src/main/java/com/example/springbootwebserver/HelloWorld.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 这是 HelloWorld 类的描述。
+ */
 @RestController
 public class HelloWorld {
     /*
@@ -13,16 +16,24 @@ public class HelloWorld {
     比如: http://localhost:8080/ 和 http://localhost:8080/index.html(两个路径访问的是同一个资源)
     比如: http://localhost:8080/website/index.html
      */
+    /**
+     * 返回 "Hello, World!" 字符串的方法。
+     */
     @GetMapping("/hello1")
     public String helloWorld1() {
         return "hello world----@Controller----@GetMapping(\"/hello1\")";
     }
-
+    /**
+     * 返回 "Hello, World!" 字符串的方法。
+     */
     @GetMapping("/hello2")
     public String helloWorld2() {
         return "hello world----@Controller----@GetMapping(\"/hello2\")";
     }
 
+    /**
+     * 返回 "Hello, World!" 字符串的方法。
+     */
     @GetMapping("/hello3")
     public String helloWorld3() {
         return "hello world----@Controller----@GetMapping(\"/hello3\")";
